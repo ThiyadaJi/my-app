585aaeee450777abbb05969890bc4c5b6e7e664a
ThiyadaJi <thiyada.ji.65@ubu.ac.th>
2022-07-18 10:51:52 +0700

    add Header and footer

---
+++ src/MyFooter.js
@@ -0,0 +1,8 @@
+function MyFooter(){
+    return (
+      <div className='My-footer'>
+        <span>Developed by Thiyada J. @ 2022</span>
+      </div>
+    );
+  }
+  export default MyFooter;
---
+++ src/Myheader.js
@@ -0,0 +1,9 @@
+function MyHeader(){
+    return (
+      <div className='My-header'>
+        <h1>React Demo Site</h1>
+      </div>
+    );
+  }
+
+export default MyHeader;
